5ec4c516a5dc56c44c68b44bd892e6f2a90b0baf
LuisEstrada33 <43280434+LuisEstrada33@users.noreply.github.com>
2018-09-28 10:29:19 -0700

    Add files via upload

---
+++ Recursividad/recursividad1/recursividad1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Recursividad1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch tiempo = Stopwatch.StartNew();
+            int resultado = 1;
+            Console.Write("Que numero factorial quiere saber: ");
+            int variable;
+            variable = int.Parse(Console.ReadLine());
+            for (int contador = 1; contador < variable + 1; contador++)
+                resultado = resultado * contador;
+            Console.Write("El resultado de {0}! es: {1}", variable, resultado);
+            Console.WriteLine("\nTiempo: " + tiempo.ElapsedMilliseconds.ToString() + " milisegundos");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Recursividad/recursividad2/recursividad2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Recursividad
+{
+    class Program
+
+    {
+
+        static int Factorial(int variable)
+        {
+
+            if (variable == 0)
+                return 1;
+            return variable * Factorial(variable - 1);
+
+        }
+        static void Main(string[] args)
+        {
+            Stopwatch tiempo = Stopwatch.StartNew();
+            Console.Write("Que numero factorial quiere ver: ");
+            int variable = int.Parse(Console.ReadLine());
+            Factorial(variable);
+            Console.WriteLine("El resultado de {0}! es: {1}", variable, Factorial(variable));
+            Console.WriteLine("\nTiempo: " + tiempo.ElapsedMilliseconds.ToString() + " milisegundos");
+            Console.ReadKey();
+        }
+    }
+}
+
