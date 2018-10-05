1ba89a684ecca6e45651ab52cd3ae5d9295135fc
LuisEstrada33 <43280434+LuisEstrada33@users.noreply.github.com>
2018-10-05 12:44:00 -0700

    Add files via upload

---
+++ U3-1/U3-1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace U3_1
+{
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Coleccion nombres = new Coleccion();
+            nombres.Add("Cesar");
+            nombres.Add("Julio");
+            nombres.Add("Arturo");
+            nombres.Add(2);
+
+            foreach (var item in nombres)
+            {
+                Console.WriteLine(item);
+
+            }
+            Console.ReadKey();
+            Console.WriteLine(nombres.Count());
+            Console.ReadKey();
+
+            nombres.Remove("Arturo");
+            Console.WriteLine(nombres.Count());
+            Console.ReadKey();
+            nombres.Clear();
+            Console.WriteLine(nombres.Count());
+            Console.ReadKey();
+
+            ArrayList lista = new ArrayList();
+            lista.Add("Pedro");
+            lista.Add("Goku");
+            lista.Add("Jiren");
+            lista.Add("Raso");
+
+            if (lista.Contains("Pedro"))
+            {
+                Console.WriteLine("Si aparece aqui");
+            }
+            Console.ReadKey();
+            lista.Sort();
+
+            foreach (var item in lista)
+            {
+                Console.WriteLine(item);
+
+            }
+            Console.ReadKey();
+            lista.Reverse();
+            foreach (var item in lista)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
